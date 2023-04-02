3083a2dfc58da5cfe60842a2650a7ae1e3da4b93
tomioM <tomiowmiyagawa@gmail.com>
2023-04-02 00:30:51 -0700

    Refine Quiz, Implement service cards

--- docs/app.js
+++ docs/app.js
@@ -1,73 +1,143 @@
 'use strict';
 
-import data from "./normalized_data.json" assert { type: "json" };
+import quizData from "./quiz-data.json" assert { type: "json" };
+import servicesData from "./services.json" assert { type: "json" };
+
+
+console.log(servicesData)
+const services = servicesData.services;
+let serviceCardsHTML = '';
+
+services.forEach(service => {
+  serviceCardsHTML += `
+    <div class="service-card" id="${service.id}">
+        <div>
+            <img class="service-card__image" src="${service.image}" alt="${service.name}">
+            <h3>${service.name}</h3>
+            <p>Price: $${service.price}</p>
+            <p>${service.description}</p>
+        </div>
+        <button class="button button--add">Add to calculator</button>
+    </div>
+  `;
+});
+
+console.log(serviceCardsHTML);
+
+const serviceCardContainerElm = document.getElementById('service-card-container');
+console.log(serviceCardContainerElm);
+serviceCardContainerElm.innerHTML = serviceCardsHTML;
 
 
 
-// - Manual and Manipulative Therapy
-// Intramuscular Stimulation (IMS)
-// Acupuncture
-// Treatment for TMJ Dysfunction
-// Sports Therapy
 
 
-let availableAnswers = [];
+// QUIZ LOGIC
+
+let displayAnswers = [];
 let output = [];
-let currentQuestion = 0;
 let answerPoints = [ 0, 0, 0, 0, 0, 0, 0 ];
+let currentQuestion = 0;
+let userAnswers = [];
 
-const quizContainer = document.getElementById('quiz-questions');
-const questionsArr = data.questions;
-const nextBtn = document.querySelector('#next');
+const formElm = document.getElementById('quiz');
+const quizDataElm = document.getElementById('quiz-data');
+const questionsArr = quizData.questions;
+const nextBtn = document.getElementById('next');
+const backBtn = document.getElementById('back');
+const stepTextElm = document.getElementById('step-text');
+const progressLineElm = document.querySelector('.progress-bar-line');
+
 nextBtn.addEventListener("click", nextQuestion);
+backBtn.addEventListener("click", previousQuestion);
+
+formElm.addEventListener('submit', e => {
+    e.preventDefault();
+});
+
+quizDataElm.addEventListener('click', event => {
+    const target = event.target;
+    if (target.type === 'radio') {
+      const userAnswer = target.value;
+      userAnswers[currentQuestion] = userAnswer;
+    }
+});
 
 
-populateQuiz();
+updateQuizState();
 
-function populateQuiz() {
+function updateQuizState() {
+    resetQuiz();
+    if (currentQuestion <= 0) {
+        backBtn.style.visibility = 'hidden';
+    } else {
+        backBtn.style.visibility = 'visible';
+    }
+
+    stepTextElm.innerHTML = `Step ${currentQuestion + 1} of ${questionsArr.length + 1}`;
+    progressLineElm.style.width = `${(currentQuestion / questionsArr.length) * 100}%`;
+
     const questionObj = questionsArr[currentQuestion];
 
     questionObj.answers.forEach(answerObj => {
-        availableAnswers.push(answerObj.answer);
+        displayAnswers.push(answerObj.answer);
     });
 
     output.push(`<h3>${questionObj.question}</h3>`);
 
-    availableAnswers.forEach((answer, i) => {
-        output.push(`<input type="radio" id="${i}" name="answer" value="${i}"><label for="${i}">${answer}</label><br>`);
+    displayAnswers.forEach((answer, i) => {
+        // if the user selected this answer previously, give that answer the checked attribute
+        if (i == userAnswers[currentQuestion]) {
+            output.push(`<input type="radio" id="${i}" name="answer" value="${i}" checked><label for="${i}">${answer}</label><br>`);
+        } else {
+            output.push(`<input type="radio" id="${i}" name="answer" value="${i}"><label for="${i}">${answer}</label><br>`);
+        }
     });
 
-    quizContainer.innerHTML = output.join('');
+    quizDataElm.innerHTML = output.join('');
 }
 
-console.log(questionsArr.length);
+
 
 function nextQuestion() {
+    currentQuestion++;
 
-    console.log(document.querySelector('input[name="answer"]:checked'));
-    const userAnswer = document.querySelector('input[name="answer"]:checked').value;
-
-    questionsArr[currentQuestion].answers[userAnswer].points.forEach((answerPoint, i) => {
-        console.log(typeof answerPoint)
-        answerPoints[i] += answerPoint;
-    })
-
-    console.log(answerPoints)
-    console.log(currentQuestion);
-    quizContainer.innerHTML = "";
-    availableAnswers = [];
-    output = [];
-
-    if (currentQuestion >= questionsArr.length -1) {
-        console.log("final result");
-        getFinalResult()
+    if (currentQuestion >= questionsArr.length) {
+        getQuizResult()
     } else {
-        currentQuestion ++;
-        populateQuiz();
+        updateQuizState();
     }
 }
 
-function getFinalResult() {
+function previousQuestion() {
+    currentQuestion--;
+    updateQuizState();
+}
+
+
+function resetQuiz() {
+    quizDataElm.innerHTML = "";
+    displayAnswers = [];
+    output = [];
+}
+
+
+function calculatePoints() {
+    userAnswers.forEach((answer, i) => {
+        const answerPointsArr = questionsArr[i].answers[answer].points;
+        answerPointsArr.forEach((answerPoint, i) => {
+            answerPoints[i] += answerPoint;
+        })
+    })
+}
+
+function getQuizResult() {
+    console.log(userAnswers);
+    stepTextElm.innerHTML = `Step ${questionsArr.length + 1} of ${questionsArr.length + 1}`;
+    progressLineElm.style.width = '100%';
+
+    calculatePoints();
+
     let largest = answerPoints[0];
     let largestIndex = 0;
     for (let i = 0; i < answerPoints.length; i++) {
@@ -78,41 +148,7 @@ function getFinalResult() {
     }
 
     console.log(largestIndex);
-    quizContainer.innerHTML = data.treatments[largestIndex].name + "\n \n" + data.treatments[largestIndex].description;
-}
+    quizDataElm.innerHTML = `<h3>${quizData.treatments[largestIndex].name}</h3> <p>${quizData.treatments[largestIndex].description}</p>`;
 
-
-
-
-
-
-
-
-
-
-
-
-
-function getAnswers(question) {
-    return question.answers.map(answer => {return "\n" + answer.answer});
-}
-
-function runPromptQuiz() {
-    questionsArr.forEach(question =>{
-        let answerIndex;
-        // Convert to int
-        answerIndex = (prompt(question.question + getAnswers(question)));
-        console.log(answerIndex);
-        console.log(question.answers[answerIndex]);
-        question.answers[answerIndex].points.forEach((answerPoint, index) => {
-            console.log(typeof answerPoint)
-            answerPoints[index] += answerPoint;
-        })
-        console.log(answerPoints)
-    })
-
-
-
-    console.log(largestIndex);
-    alert(data.treatments[largestIndex].name + "\n \n" + data.treatments[largestIndex].description);
+    document.getElementById(largestIndex).scrollIntoView({behavior: 'smooth'});
 }
