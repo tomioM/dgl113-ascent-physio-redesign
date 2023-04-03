f54bda2c1009a7fb5650c635198c36376791d7c6
tomioM <tomiowmiyagawa@gmail.com>
2023-04-03 12:07:40 -0700

    Format with VScode js Language Features

--- docs/app.js
+++ docs/app.js
@@ -9,6 +9,7 @@ import servicesData from "./services.json" assert { type: "json" };
 const services = servicesData.services;
 const addBtnPhrase = "Add to invoice";
 const removeBtnPhrase = "Remove from invoice";
+const pricePrefix = "$"
 let servicesInCart = [];
 
 // Element References
@@ -18,73 +19,73 @@ const pstInvoiceElm = document.getElementById("pst");
 const totalInvoiceElm = document.getElementById("total");
 
 function manageInvoiceBtnClick(e) {
-  /* Gets the string contained in the clicked button elements data-index attribute. 
-    This is the index of the associated service in the services array */
-  const index = e.target.getAttribute("data-index");
+    /* Gets the string contained in the clicked button elements data-index attribute. 
+      This is the index of the associated service in the services array */
+    const index = e.target.getAttribute("data-index");
 
-  // Take the action indicated by the button state then toggles the content of the the button between two phrases to reflect the action it will take next
-  if (e.target.classList.contains("button--remove")) {
-    e.target.innerHTML = addBtnPhrase;
-    removeServiceFromCalculator(index);
-  } else {
-    e.target.innerHTML = removeBtnPhrase;
-    addServiceToCalculator(index);
-  }
+    // Take the action indicated by the button state then toggles the content of the the button between two phrases to reflect the action it will take next
+    if (e.target.classList.contains("button--remove")) {
+        e.target.innerHTML = addBtnPhrase;
+        removeServiceFromCalculator(index);
+    } else {
+        e.target.innerHTML = removeBtnPhrase;
+        addServiceToCalculator(index);
+    }
 
-  // Toggle the "button-remove" class. (less code then adding / removing in the above step)
-  e.target.classList.toggle("button--remove");
+    // Toggle the "button-remove" class. (less code then adding / removing in the above step)
+    e.target.classList.toggle("button--remove");
 }
 
 function addServiceToCalculator(index) {
-  // Add the relevant information of the service in which the button was clicked on to the beginning of the "servicesInCart" Array
-  servicesInCart.unshift({
-    name: services[index].name,
-    price: services[index].price,
-    index: index,
-  });
-  outputToHTML(buildInvoiceItems(), invoiceDataElm);
+    // Add the relevant information of the service in which the button was clicked on to the beginning of the "servicesInCart" Array
+    servicesInCart.unshift({
+        name: services[index].name,
+        price: services[index].price,
+        index: index,
+    });
+    outputToHTML(buildInvoiceItems(), invoiceDataElm);
 }
 
 function removeServiceFromCalculator(index) {
-  // Remove the serviceInCart with the index of the service in which the button was clicked
-  servicesInCart = servicesInCart.filter((item) => {
-    return item.index !== index;
-  });
+    // Remove the serviceInCart with the index of the service in which the button was clicked
+    servicesInCart = servicesInCart.filter((item) => {
+        return item.index !== index;
+    });
 
-  outputToHTML(buildInvoiceItems(), invoiceDataElm);
+    outputToHTML(buildInvoiceItems(), invoiceDataElm);
 }
 
 function buildInvoiceItems() {
-  let output = [];
+    let output = [];
 
-  updateInvoiceTotals();
+    updateInvoiceTotals();
 
-  servicesInCart.forEach((item) => {
-    output.push(
-      `<div class="invoice-name-price">${item.name}<p></p><p>${item.price}</p></div>`
-    );
-  });
+    servicesInCart.forEach((item) => {
+        output.push(
+            `<div class="invoice-name-price">${item.name}<p></p><p>${pricePrefix + item.price}</p></div>`
+        );
+    });
 
-  return output;
+    return output;
 }
 
 function updateInvoiceTotals() {
-  let total = 0;
-  for (const item of servicesInCart) {
-    total += item.price;
-  }
+    let total = 0;
+    for (const item of servicesInCart) {
+        total += item.price;
+    }
 
-  const gst = roundMoney(total * 0.05);
-  const pst = roundMoney(total * 0.07);
-  const taxTotal = roundMoney(total + gst + pst);
+    const gst = roundMoney(total * 0.05);
+    const pst = roundMoney(total * 0.07);
+    const taxTotal = roundMoney(total + gst + pst);
 
-  totalInvoiceElm.innerHTML = taxTotal;
-  gstInvoiceElm.innerHTML = gst;
-  pstInvoiceElm.innerHTML = pst;
+    totalInvoiceElm.innerHTML = pricePrefix + taxTotal;
+    gstInvoiceElm.innerHTML = pricePrefix + gst;
+    pstInvoiceElm.innerHTML = pricePrefix + pst;
 }
 
 function roundMoney(num) {
-  return Math.ceil(num * 100) / 100;
+    return Math.ceil(num * 100) / 100;
 }
 
 // FILTERING
@@ -92,7 +93,7 @@ function roundMoney(num) {
 // Element References
 const filterBtns = document.querySelectorAll("button.filter-chips__button");
 const serviceCardContainerElm = document.getElementById(
-  "service-card-container"
+    "service-card-container"
 );
 
 // Initial cards
@@ -100,55 +101,59 @@ outputToHTML(buildServiceCards(services), serviceCardContainerElm);
 addlistenerToBtns();
 
 filterBtns.forEach((filterBtn) => {
-  filterBtn.addEventListener("click", manageFilterBtnClick);
+    filterBtn.addEventListener("click", manageFilterBtnClick);
 });
 
 function manageFilterBtnClick(e) {
-  const activeBtn = document.querySelector(".filter-chips__button--active");
-  activeBtn.classList.remove("filter-chips__button--active");
-  e.target.classList.add("filter-chips__button--active");
+    const activeBtn = document.querySelector(".filter-chips__button--active");
+    activeBtn.classList.remove("filter-chips__button--active");
+    e.target.classList.add("filter-chips__button--active");
 
-  const filterStr = e.target.getAttribute("data-filter");
-  if (filterStr == "all") {
-    outputToHTML(buildServiceCards(services), serviceCardContainerElm);
-    addlistenerToBtns();
-  } else {
-    const filteredServices = services.filter((service) =>
-      service.tags.includes(filterStr)
-    );
-    outputToHTML(buildServiceCards(filteredServices), serviceCardContainerElm);
-    addlistenerToBtns();
-  }
+    const filterStr = e.target.getAttribute("data-filter");
+    if (filterStr == "all") {
+        outputToHTML(buildServiceCards(services), serviceCardContainerElm);
+        addlistenerToBtns();
+    } else {
+        const filteredServices = services.filter((service) =>
+            service.tags.includes(filterStr)
+        );
+        outputToHTML(buildServiceCards(filteredServices), serviceCardContainerElm);
+        addlistenerToBtns();
+    }
 }
 
 function buildServiceCards(filteredServices) {
-  let output = [];
+    let output = [];
 
-  filteredServices.forEach((service) => {
-    const isInCart = servicesInCart.find((serviceInCart) => {
-      return serviceInCart.index == service.index;
-    });
+    filteredServices.forEach((service) => {
+        const isInCart = servicesInCart.find((serviceInCart) => {
+            return serviceInCart.index == service.index;
+        });
 
-    output.push(`
+        output.push(`
             <div class="service-card" id="${service.id}">
                 <div>
-                    <img class="service-card__image" src="${
-                      service.image
-                    }" alt="${service.name}">
-                    <p class="service-card__price">Price: $${service.price}</p>
+                    <img class="service-card__image" src="${service.image
+            }" alt="${service.name}">
+                    <p class="service-card__price">Price: ${pricePrefix + service.price}</p>
                     <h3>${service.name}</h3>
                     <p>${service.description}</p>
                 </div>
-                <button class="button button--invoice ${
-                  isInCart ? "button--remove" : ""
-                }" data-index="${service.index}">${
-      isInCart ? removeBtnPhrase : addBtnPhrase
-    }</button>
+                <button class="button button--invoice ${isInCart ? "button--remove" : ""
+            }" data-index="${service.index}">${isInCart ? removeBtnPhrase : addBtnPhrase
+            }</button>
             </div>
         `);
-  });
+    });
 
-  return output;
+    return output;
+}
+
+function addlistenerToBtns() {
+    const addBtns = document.querySelectorAll('.button--invoice');
+    addBtns.forEach(addBtn => {
+        addBtn.addEventListener('click', manageInvoiceBtnClick);
+    })
 }
 
 // QUIZ
@@ -178,165 +183,164 @@ startQuizBtn.addEventListener("click", startQuiz);
 
 // Prevent default form behavior
 formElm.addEventListener("submit", (e) => {
-  e.preventDefault();
+    e.preventDefault();
 });
 
 // Add event listener to quiz data container which updates the userAnswer variable to the currently checked radio input
 quizDataElm.addEventListener("click", (event) => {
-  const target = event.target;
-  if (target.type === "radio") {
-    const userAnswer = target.value;
-    userAnswers[currentQuestion] = userAnswer;
-  }
+    const target = event.target;
+    if (target.type === "radio") {
+        const userAnswer = target.value;
+        userAnswers[currentQuestion] = userAnswer;
+    }
 });
 
 // Transitions away from start quiz screen and updates the quizzes state.
 function startQuiz() {
-  startQuizScreenElm.style.opacity = 0;
-  formElm.style.height = "520px";
+    startQuizScreenElm.style.opacity = 0;
+    formElm.style.height = "520px";
 
-  setTimeout(() => {
-    startQuizScreenElm.style.display = "none";
-    formElm.style.overflow = "auto";
-    buildQuiz();
-  }, 300);
+    setTimeout(() => {
+        startQuizScreenElm.style.display = "none";
+        formElm.style.overflow = "auto";
+        outputToHTML(buildQuiz(), quizDataElm);
+    }, 300);
 }
 
 // Increment current question, get results, update quiz state
 function nextQuestion() {
-  currentQuestion++;
+    currentQuestion++;
 
-  if (currentQuestion >= totalQuestions) {
-    outputToHTML(buildQuizResult(), quizDataElm);
-    nextBtn.style.visibility = "hidden";
-    backBtn.value = "Reset Quiz";
-  } else {
-    outputToHTML(buildQuiz(), quizDataElm);
-  }
+    if (currentQuestion >= totalQuestions) {
+        outputToHTML(buildQuizResult(), quizDataElm);
+        nextBtn.style.visibility = "hidden";
+        backBtn.value = "Reset Quiz";
+    } else {
+        outputToHTML(buildQuiz(), quizDataElm);
+    }
 }
 
 // Decrement current question, reset, update quiz state
 function previousQuestion() {
-  // If current step is the final then Reset variables to default
-  if (currentQuestion >= totalQuestions) {
-    nextBtn.style.visibility = "visible";
-    backBtn.value = "Back";
+    // If current step is the final then Reset variables to default
+    if (currentQuestion >= totalQuestions) {
+        nextBtn.style.visibility = "visible";
+        backBtn.value = "Back";
 
-    answerPoints = [0, 0, 0, 0, 0, 0, 0];
-    currentQuestion = 0;
-    userAnswers = [];
-  } else {
-    currentQuestion--;
-  }
+        answerPoints = [0, 0, 0, 0, 0, 0, 0];
+        currentQuestion = 0;
+        userAnswers = [];
+    } else {
+        currentQuestion--;
+    }
 
-  outputToHTML(buildQuiz(), quizDataElm);
+    outputToHTML(buildQuiz(), quizDataElm);
 }
 
 // Builds html to reflect the various variables that control the quiz
 function buildQuiz() {
-  let output = [];
-  let currentAnswers = [];
+    let output = [];
+    let currentAnswers = [];
 
-  // If one the first step, hide the back button
-  if (currentQuestion <= 0) {
-    backBtn.style.visibility = "hidden";
-  } else {
-    backBtn.style.visibility = "visible";
-  }
-
-  // Update the step text and the progress bar
-  stepTextElm.innerHTML = `Step ${currentQuestion + 1} of ${
-    totalQuestions + 1
-  }`;
-  progressLineElm.style.width = `${(currentQuestion / totalQuestions) * 100}%`;
-
-  // Instantiate the question variable with the current question object
-  const question = quizData.questions[currentQuestion];
-
-  // Push the question string onto the output array
-  output.push(`<h3>${question.question}</h3>`);
-
-  // Pushes the answer strings onto the currentAnswers array
-  question.answers.forEach((answer) => {
-    currentAnswers.push(answer.answer);
-  });
-
-  // Push the HTML code for elements using the available answer data onto the output array
-  currentAnswers.forEach((answer, i) => {
-    // If the user selected this answer previously, give that answer the checked attribute
-    if (i == userAnswers[currentQuestion]) {
-      output.push(
-        `<input type="radio" id="${i}" name="answer" value="${i}" checked><label for="${i}">${answer}</label><br>`
-      );
+    // If one the first step, hide the back button
+    if (currentQuestion <= 0) {
+        backBtn.style.visibility = "hidden";
     } else {
-      output.push(
-        `<input type="radio" id="${i}" name="answer" value="${i}"><label for="${i}">${answer}</label><br>`
-      );
+        backBtn.style.visibility = "visible";
     }
-  });
 
-  return output;
+    // Update the step text and the progress bar
+    stepTextElm.innerHTML = `Step ${currentQuestion + 1} of ${totalQuestions + 1
+        }`;
+    progressLineElm.style.width = `${(currentQuestion / totalQuestions) * 100}%`;
+
+    // Instantiate the question variable with the current question object
+    const question = quizData.questions[currentQuestion];
+
+    // Push the question string onto the output array
+    output.push(`<h3>${question.question}</h3>`);
+
+    // Pushes the answer strings onto the currentAnswers array
+    question.answers.forEach((answer) => {
+        currentAnswers.push(answer.answer);
+    });
+
+    // Push the HTML code for elements using the available answer data onto the output array
+    currentAnswers.forEach((answer, i) => {
+        // If the user selected this answer previously, give that answer the checked attribute
+        if (i == userAnswers[currentQuestion]) {
+            output.push(
+                `<input type="radio" id="${i}" name="answer" value="${i}" checked><label for="${i}">${answer}</label><br>`
+            );
+        } else {
+            output.push(
+                `<input type="radio" id="${i}" name="answer" value="${i}"><label for="${i}">${answer}</label><br>`
+            );
+        }
+    });
+
+    return output;
 }
 
 // Calculate winning diagnosis and change quiz to reflect
 function buildQuizResult() {
-  let output = [];
+    let output = [];
 
-  stepTextElm.innerHTML = `Step ${totalQuestions + 1} of ${totalQuestions + 1}`;
-  progressLineElm.style.width = "100%";
+    stepTextElm.innerHTML = `Step ${totalQuestions + 1} of ${totalQuestions + 1}`;
+    progressLineElm.style.width = "100%";
 
-  const winningServiceIndex = getWinningIndex();
+    const winningServiceIndex = getWinningIndex();
 
-  // Separating the code for each html element is more readable and looks nice when put through the outputToHTML method
-  output.push(`<h3>${quizData.treatments[winningServiceIndex].name}</h3>`);
-  output.push(`<p>${quizData.treatments[winningServiceIndex].description}</p>`);
-  output.push(
-    `<a href="#${servicesData.services[winningServiceIndex].id}">Scroll to ${quizData.treatments[largestIndex].name}</a>`
-  );
+    // Separating the code for each html element is more readable and looks nice when put through the outputToHTML method
+    output.push(`<h3>${quizData.treatments[winningServiceIndex].name}</h3>`);
+    output.push(`<p>${quizData.treatments[winningServiceIndex].description}</p>`);
+    output.push(
+        `<a href="#${servicesData.services[winningServiceIndex].id}">Scroll to ${quizData.treatments[largestIndex].name}</a>`
+    );
 
-  return output;
+    return output;
 }
 
 // Calculate the points based on the userAnswers array
 function getWinningIndex() {
-  userAnswers.forEach((answer, i) => {
-    const answerPointsArr = quizData.questions[i].answers[answer].points;
-    answerPointsArr.forEach((answerPoint, i) => {
-      answerPoints[i] += answerPoint;
+    userAnswers.forEach((answer, i) => {
+        const answerPointsArr = quizData.questions[i].answers[answer].points;
+        answerPointsArr.forEach((answerPoint, i) => {
+            answerPoints[i] += answerPoint;
+        });
     });
-  });
 
-  // "..." Spreads the answerPoints array into separate arguments which the Math.max function requires
-  let largest = Math.max(...answerPoints);
-  return answerPoints.indexOf(largest);
+    // "..." Spreads the answerPoints array into separate arguments which the Math.max function requires
+    let largest = Math.max(...answerPoints);
+    return answerPoints.indexOf(largest);
 }
 
 // Given the an array of strings containing code for html elements and a reference to a parent in which to append the output. Each element of the output array is added consecutively with a delay and transition (because it looks good)
 function outputToHTML(output, parentElem) {
-  // Remove children in parent before adding the new elements
-  removeAllChildNodes(parentElem);
-  let delay = 150;
+    // Remove children in parent before adding the new elements
+    removeAllChildNodes(parentElem);
+    let delay = 150;
 
-  output.forEach((element, index) => {
-    const div = document.createElement("div");
-    div.innerHTML = element;
-    div.style.opacity = 0;
-    div.style.transition = `opacity ${delay}ms linear`;
+    output.forEach((element, index) => {
+        const div = document.createElement("div");
+        div.innerHTML = element;
+        div.style.opacity = 0;
+        div.style.transition = `opacity ${delay}ms linear`;
 
-    parentElem.appendChild(div);
+        parentElem.appendChild(div);
 
-    setTimeout(() => {
-      div.style.opacity = 1;
-    }, delay);
+        setTimeout(() => {
+            div.style.opacity = 1;
+        }, delay);
 
-    // Increase speed of transition for each element. I think it looks more pleasing
-    delay += 80 * Math.pow(0.8, index);
-  });
+        // Increase speed of transition for each element. I think it looks more pleasing
+        delay += 80 * Math.pow(0.8, index);
+    });
 }
 
 // Removes all child nodes of a parent
 function removeAllChildNodes(parent) {
-  while (parent.firstChild) {
-    parent.removeChild(parent.firstChild);
-  }
+    while (parent.firstChild) {
+        parent.removeChild(parent.firstChild);
+    }
 }
